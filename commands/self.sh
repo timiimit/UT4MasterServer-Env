46799bceb6341de0e9bae23ad9acf892a28ce275
timiimit <timi.korda@gmail.com>
2023-09-03 11:13:27 +0200

    Implement self update command

--- commands/self.sh
+++ commands/self.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
-
+if [ "$1" == "update" ]; then
+	if [ -z "$2" ]; then
+		git -C $ROOT_DIR_ENV pull
+	else
+		echo "Description:"
+		echo "Update \`$SCRIPT_COMMAND\` command."
+		echo ""
+		echo "Syntax:"
+		echo "	$SCRIPT_COMMAND self update"
+	fi
 else
 	echo "Description:"
 	echo "Perform operation which handles the \`$SCRIPT_COMMAND\` command itself."
 	echo ""
+	echo "Syntax:"
+	echo "	$SCRIPT_COMMAND self <command> ..."
 	echo ""
+	echo "Commands:"
+	echo "	update"
 fi
