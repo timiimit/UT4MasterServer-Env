0fe5fd00ec94966dd5d335c9ca052f945a8b8add
timiimit <timi.korda@gmail.com>
2023-09-04 16:21:46 +0200

    Simplify self update command

--- commands/self.sh
+++ commands/self.sh
@@ -5,9 +5,13 @@ systemd_dir="/etc/systemd/system"
 
 if [ "$1" == "update" ]; then
 	if [ -z "$2" ]; then
+
+		# navigate into env repo for easier operation
+		cd "$ROOT_DIR_ENV"
+
 		# capture output while also displaying it to console
 		exec 5>&1
-		fetch_output="$(git -C "$ROOT_DIR_ENV" fetch | tee /dev/fd/5)"
+		fetch_output="$(git fetch | tee /dev/fd/5)"
 		exec 5<&-
 
 		if [ -z "$fetch_output" ]; then
@@ -15,12 +19,12 @@ if [ "$1" == "update" ]; then
 			exit
 		fi
 
-		"$ROOT_DIR_ENV/ut4ms.sh" self uninstall
-		git -C "$ROOT_DIR_ENV" stash push config.cfg 1>/dev/null
-		git -C "$ROOT_DIR_ENV" checkout -f HEAD 1>/dev/null
-		git -C "$ROOT_DIR_ENV" pull
-		git -C "$ROOT_DIR_ENV" stash pop 1>/dev/null
-		"$ROOT_DIR_ENV/ut4ms.sh" self install
+		"./ut4ms.sh" self uninstall
+		git stash push config.cfg 1>/dev/null
+		git checkout -f HEAD 1>/dev/null
+		git pull
+		git stash pop 1>/dev/null
+		"./ut4ms.sh" self install
 	else
 		echo "Description:"
 		echo "Update \`$SCRIPT_COMMAND\` command."
