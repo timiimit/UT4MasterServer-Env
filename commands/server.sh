29cb2cf4b65ff6afcb25651c2065de4ca450a078
timiimit <timi.korda@gmail.com>
2023-09-05 13:00:55 +0200

    Add git checks to server update command

--- commands/server.sh
+++ commands/server.sh
@@ -24,6 +24,52 @@ elif [ "$1" == "reload" ]; then
 		exit
 	fi
 
+	# docker-compose -f docker-compose.yml down
+	# docker system prune -af
+	docker-compose -f docker-compose.yml up --build -d
+	docker system prune -af
+elif [ "$1" == "update" ]; then
+
+	if [ -z "$IS_SERVICE" ]; then
+		echo "Do not run this command manually. Please use \`systemctl start ut4ms_update\` instead."
+		exit
+	fi
+
+	# in case repo is already there, just pull latest code, else clone fresh
+	if [ "$(git remote get-url origin 2>/dev/null)" == "$REPO_URL_APP" ]; then
+
+		# minimal fetch
+		git remote update
+
+		# compare local commit id and remote commit id
+		COMMIT_UPSTREAM=$(git rev-parse @{u})
+		COMMIT_LOCAL=$(git rev-parse @)
+
+		if [ "$COMMIT_UPSTREAM" == "$COMMIT_LOCAL" ]; then
+			echo "Already up to date."
+			exit 1
+		fi
+
+		# pull all changes
+		git pull
+
+		# switch to desired branch
+		if [ "$(git branch --show-current)" != "$REPO_BRANCH_ENV"]; then
+			git checkout -f "$REPO_BRANCH_ENV" 1>/dev/null
+		fi
+	else
+		# make sure that we are cloning into an empty directory
+		if [ -z "$(\ls -A)" ]; then
+			git clone -b "$REPO_BRANCH_APP" "$REPO_URL_APP" .
+		else
+			echo "\`$(pwd)\` is not empty and it doesn't contain the right"
+			echo "repository. Unsure what to do."
+			echo ""
+			echo "Stopping."
+			exit
+		fi
+	fi
+
 cat << EOF > UT4MasterServer/appsettings.Production.json
 {
 	"ApplicationSettings": {
@@ -41,34 +87,6 @@ VITE_API_URL=https://$DOMAIN_NAME_API
 VITE_BASIC_AUTH="basic MzRhMDJjZjhmNDQxNGUyOWIxNTkyMTg3NmRhMzZmOWE6ZGFhZmJjY2M3Mzc3NDUwMzlkZmZlNTNkOTRmYzc2Y2Y="
 VITE_RECAPTCHA_SITE_KEY=$RECAPTCHA_SITE_KEY
 EOF
-
-	# docker-compose -f docker-compose.yml down
-	# docker system prune -af
-	docker-compose -f docker-compose.yml up --build -d
-	docker system prune -af
-elif [ "$1" == "update" ]; then
-
-	if [ -z "$IS_SERVICE" ]; then
-		echo "Do not run this command manually. Please use \`systemctl start ut4ms_update\` instead."
-		exit
-	fi
-
-	# in case repo is already there, just pull latest code, else clone fresh
-	if [ "$(git remote get-url origin 2>/dev/null)" == "$REPO_URL_APP" ]; then
-		git pull
-		git checkout -f "$REPO_BRANCH_APP" 1>/dev/null
-	else
-		# make sure that we are cloning into an empty directory
-		if [ -z "$(\ls -A)" ]; then
-			git clone -b "$REPO_BRANCH_APP" "$REPO_URL_APP" .
-		else
-			echo "\`$(pwd)\` is not empty and it doesn't contain the right"
-			echo "repository. Unsure what to do."
-			echo ""
-			echo "Stopping."
-			exit
-		fi
-	fi
 
 	echo "Server successfully updated. Reload server to apply changes."
 else
