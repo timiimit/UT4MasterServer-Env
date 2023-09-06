b1f602e67c53acbe2d0e0acecee85ca1135d4c99
timiimit <timi.korda@gmail.com>
2023-09-06 14:07:40 +0200

    Make cert commands more robust

--- commands/cert.sh
+++ commands/cert.sh
@@ -1,10 +1,45 @@
 #!/bin/sh
 
+if [ "$2" == "--dry-run" ]; then
+	opt_dryrun="--dry-run"
+fi
+
 if [ "$1" == "obtain" ]; then
+	if [ -n "$2" ]; then
+		if [ "$2" == "--dry-run" ]; then
+			opt_dryrun="--dry-run"
+		else
+			echo "Description:"
+			echo "Obtain new SSL certificate."
+			echo ""
+			echo "Syntax:"
+			echo "	cert obtain [options]"
+			echo ""
+			echo "Options:"
+			echo "	--dry-run	Just do a test run."
+			exit
+		fi
+	fi
+
 	# obtain certificates by letting certbot use apache's document root
-	certbot certonly --email "$CERTIFICATE_REGISTRATION_EMAIL" --agree-tos --non-interactive -d "$DOMAIN_NAME_WEBSITE" -d "$DOMAIN_NAME_API" --webroot --webroot-path "/var/www/html"
+	echo certbot certonly $opt_dryrun --email "$CERTIFICATE_REGISTRATION_EMAIL" --agree-tos --non-interactive -d "$DOMAIN_NAME_WEBSITE" -d "$DOMAIN_NAME_API" --webroot --webroot-path "/var/www/html"
 elif [ "$1" == "renew" ]; then
-	certbot renew
+	if [ -n "$2" ]; then
+		if [ "$2" == "--dry-run" ]; then
+			opt_dryrun="--dry-run"
+		else
+			echo "Description:"
+			echo "Renew all old SSL certificates that are near expiry."
+			echo ""
+			echo "Syntax:"
+			echo "	cert renew [options]"
+			echo ""
+			echo "Options:"
+			echo "	--dry-run	Just do a test run."
+			exit
+		fi
+	fi
+	echo certbot renew $opt_dryrun
 else
 	echo "Description:"
 	echo "Work with certificate authority to handle/manipulate ssl certificates"
@@ -15,10 +50,10 @@ else
 	echo "	$DOMAIN_NAME_API"
 	echo ""
 	echo "Syntax:"
-	echo "	cert <command>"
+	echo "	cert <command> ..."
 	echo ""
 	echo "Commands:"
-	echo "	obtain	Obtain new certificates for configured domains."
-	echo "	renew	Renew all previously obtained certificates."
+	echo "	obtain"
+	echo "	renew"
 	exit
 fi
