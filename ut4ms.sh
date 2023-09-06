0c807555e69f230e57c63bfc4fda352b30e63e5b
timiimit <timi.korda@gmail.com>
2023-09-06 07:53:43 +0200

    Remove the need for ROOT_DIR_ENV in config

--- ut4ms.sh
+++ ut4ms.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-export SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+export ROOT_DIR_ENV="$SCRIPT_DIR"
 if [ -z "$SCRIPT_COMMAND" ]; then
 	export SCRIPT_COMMAND="$0"
 
@@ -11,17 +12,17 @@ if [ -z "$SCRIPT_COMMAND" ]; then
 fi
 
 set -a # automatically export all variables
-source $SCRIPT_DIR/config.cfg
+source $ROOT_DIR_ENV/config.cfg
 set +a
 
 cd $ROOT_DIR_APP
 
-if [ ! -f "$SCRIPT_DIR/commands/$1.sh" ]; then
+if [ ! -f "$ROOT_DIR_ENV/commands/$1.sh" ]; then
 	echo "Syntax:"
 	echo "	$SCRIPT_COMMAND <command> ..."
 	echo ""
 	echo "Commands:"
-	for file in $SCRIPT_DIR/commands/*.sh; do
+	for file in $ROOT_DIR_ENV/commands/*.sh; do
 		filename="${file##*/}"
 		echo "	${filename%.*}"
 	done
@@ -32,4 +33,4 @@ if [ ! -f "$SCRIPT_DIR/commands/$1.sh" ]; then
 	exit
 fi
 
-"$SCRIPT_DIR/commands/$1.sh" ${@:2}
+"$ROOT_DIR_ENV/commands/$1.sh" ${@:2}
